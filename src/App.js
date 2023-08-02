cd2c1c6a07ea68889b9127478103af2f69df1f95
leopantsulaia <leopantsulaia@gmail.com>
2023-08-02 15:39:40 +0400

    cool mode newer version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Palette from "./Palette";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
@@ -10,113 +9,96 @@ import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 function SingleColorPaletteWrapper(props) {
-  const params = useParams();
-  const colorId = params.colorId;
-  const paletteId = params.paletteId;
-  return (
-    <SingleColorPalette
-      colorId={colorId}
-      palette={generatePalette(props.findPalette(paletteId))}
-    />
-  );
+	const params = useParams();
+	const colorId = params.colorId;
+	const paletteId = params.paletteId;
+	return (
+		<SingleColorPalette
+			colorId={colorId}
+			palette={generatePalette(props.findPalette(paletteId))}
+		/>
+	);
 }
 
 function PaletteWrapper(props) {
-  const params = useParams();
-  const id = params.id;
-  return <Palette palette={generatePalette(props.findPalette(id))} />;
+	const params = useParams();
+	const id = params.id;
+	return <Palette palette={generatePalette(props.findPalette(id))} />;
 }
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
-    this.state = { palettes: savedPalettes || seedColors };
-    this.savePalette = this.savePalette.bind(this);
-    this.findPalette = this.findPalette.bind(this);
-    this.deletePalette = this.deletePalette.bind(this);
-  }
-  findPalette(id) {
-    return this.state.palettes.find(function (palette) {
-      return palette.id === id;
-    });
-  }
-  deletePalette(id) {
-    this.setState(
-      (st) => ({
-        palettes: st.palettes.filter((palette) => palette.id !== id),
-      }),
-      this.syncLocalStorage
-    );
-  }
-  savePalette(newPalette) {
-    this.setState(
-      { palettes: [...this.state.palettes, newPalette] },
-      this.syncLocalStorage
-    );
-  }
-  syncLocalStorage() {
-    //save palettes to local storage
-    window.localStorage.setItem("palettes", JSON.stringify(this.state.palettes));
-  }
+function App() {
+	const [palettes, setPalettes] = useState(JSON.parse(window.localStorage.getItem("palettes")) || seedColors);
 
-  render() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/palette/new"
-            element={
-              <Page>
-                <NewPaletteForm
-                  savePalette={this.savePalette}
-                  palettes={this.state.palettes}
-                />
-              </Page>
-            }
-          />
-          <Route
-            path="/palette/:paletteId/:colorId"
-            element={
-              <Page>
-                <SingleColorPaletteWrapper findPalette={this.findPalette} />
-              </Page>
-            }
-          />
-          <Route
-            path="/"
-            element={
-              <Page>
-                <PaletteList
-                  palettes={this.state.palettes}
-                  deletePalette={this.deletePalette}
-                />
-              </Page>
-            }
-          />
-          <Route
-            path="/palette/:id"
-            element={
-              <Page>
-                <PaletteWrapper findPalette={this.findPalette} />
-              </Page>
-            }
-          />
-            <Route
-              element={
-                <Page>
-                  <PaletteList
-                    palettes={this.state.palettes}
-                    deletePalette={this.deletePalette}
-                  />
-                </Page>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      );
-    }
-  }
-  
-  export default App;
-  
+	useEffect(() => {
+		window.localStorage.setItem("palettes", JSON.stringify(palettes));
+	}, [palettes]);
+
+	const findPalette = (id) => {
+		return palettes.find((palette) => palette.id === id);
+	};
+
+	const deletePalette = (id) => {
+		setPalettes((prevPalettes) => prevPalettes.filter((palette) => palette.id !== id));
+	};
+
+	const savePalette = (newPalette) => {
+		setPalettes((prevPalettes) => [...prevPalettes, newPalette]);
+	};
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/palette/new"
+					element={
+						<Page>
+							<NewPaletteForm
+								savePalette={savePalette}
+								palettes={palettes}
+							/>
+						</Page>
+					}
+				/>
+				<Route
+					path="/palette/:paletteId/:colorId"
+					element={
+						<Page>
+							<SingleColorPaletteWrapper findPalette={findPalette} />
+						</Page>
+					}
+				/>
+				<Route
+					path="/"
+					element={
+						<Page>
+							<PaletteList
+								palettes={palettes}
+								deletePalette={deletePalette}
+							/>
+						</Page>
+					}
+				/>
+				<Route
+					path="/palette/:id"
+					element={
+						<Page>
+							<PaletteWrapper findPalette={findPalette} />
+						</Page>
+					}
+				/>
+				<Route
+					element={
+						<Page>
+							<PaletteList
+								palettes={palettes}
+								deletePalette={deletePalette}
+							/>
+						</Page>
+					}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
@@ -6,14 +6,12 @@ import styles from './styles/PaletteStyles';
 import { withStyles } from '@material-ui/styles';
 import ColorBox from './ColorBox';
 
-class SingleColorPalette extends Component {
-  constructor(props) {
-    super(props);
-    this._shades = this.gatherShades(this.props.palette, this.props.colorId);
-    this.state = { format: 'hex' };
-    this.changeFormat = this.changeFormat.bind(this);
-  }
-  gatherShades(palette, colorToFilterBy) {
+function SingleColorPalette(props) {
+  const [format, setFormat] = useState('hex');
+  const { palette, colorId, classes } = props;
+  const shades = gatherShades(palette, colorId);
+
+  function gatherShades(palette, colorToFilterBy) {
     let shades = [];
     let allColors = palette.colors;
 
@@ -24,33 +22,33 @@ class SingleColorPalette extends Component {
     }
     return shades.slice(1);
   }
-  changeFormat(val) {
-    this.setState({ format: val });
+
+  function changeFormat(val) {
+    setFormat(val);
   }
-  render() {
-    const { format } = this.state;
-    const { paletteName, emoji, id } = this.props.palette;
-    const { classes } = this.props;
-    const colorBoxes = this._shades.map((color) => (
-      <ColorBox
-        key={color.name}
-        name={color.name}
-        background={color[format]}
-        showingFullPalette={false}
-      />
-    ));
-    return (
-      <div className={classes.Palette}>
-        <Navbar handleChange={this.changeFormat} showingAllColors={false} />
-        <div className={classes.colors}>
-          {colorBoxes}
-          <div className={classes.goBack}>
-            <Link to={`/palette/${id}`}>GO BACK</Link>
-          </div>
+
+  const { paletteName, emoji } = palette;
+  const colorBoxes = shades.map((color) => (
+    <ColorBox
+      key={color.name}
+      name={color.name}
+      background={color[format]}
+      showingFullPalette={false}
+    />
+  ));
+
+  return (
+    <div className={classes.Palette}>
+      <Navbar handleChange={changeFormat} showingAllColors={false} />
+      <div className={classes.colors}>
+        {colorBoxes}
+        <div className={classes.goBack}>
+          <Link to={`/palette/`}>GO BACK</Link>
         </div>
-        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
-    );
-  }
+      <PaletteFooter paletteName={paletteName} emoji={emoji} />
+    </div>
+  );
 }
+
 export default withStyles(styles)(SingleColorPalette);
